c31b287c1429843814d5fe7b0e2a86e3971370f9
Žygimantas Medelis <zygis@hey.com>
2023-01-05 19:26:53 +0200

    drop single/batch config

--- dev/user.clj
+++ dev/user.clj
@@ -2,7 +2,7 @@
   (:require [nextjournal.clerk :as clerk]))
 
 ;; start Clerk's built-in webserver on the default port 7777, opening the browser when done
-(clerk/serve! {:browse? true})
+#_(clerk/serve! {:browse? true})
 
 ;; or let Clerk watch the given `:paths` for changes
-(clerk/serve! {:watch-paths ["src"]})
+#_(clerk/serve! {:watch-paths ["src"]})
