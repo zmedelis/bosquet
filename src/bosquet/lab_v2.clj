2f219245acd267dfdbec67be0411393de3249332
Žygimantas Medelis <zygis@hey.com>
2023-01-29 07:30:43 +0200

    custom selmer 'llm-generate' tag

--- src/bosquet/lab_v2.clj
+++ src/bosquet/lab_v2.clj
@@ -1,55 +1,17 @@
 (ns bosquet.lab-v2
   (:require
     [bosquet.template :as template]
+    [bosquet.template.tag :as tag]
     [clojure.java.io :as io]
-    [selmer.parser :as parser]
-    [selmer.util :as u]))
-
+    [selmer.parser :as parser]))
 
 (defn prompt-def []
-  (template/read-edn (io/reader "resources/pp2.edn"))
-  )
+  (template/read-edn (io/reader "resources/pp2.edn")))
 
-
-(defmacro add-tag!
-  " tag name, fn handler, and maybe tags "
-  [k handler & tags]
-  `(do
-     (parser/set-closing-tags! ~k ~@tags)
-     (swap! selmer.tags/expr-tags assoc ~k (tag-handler ~handler ~k ~@tags))))
-
-(parser/add-tag! :foo
-  (fn [args context-map]
-    (prn "ARGS " args)
-    (prn "CM   " context-map)
-    (str "foo " (first args))))
-
-
-(defn render-template
-  " vector of ^selmer.node.INodes and a context map."
-  [template context-map]
-  (let [buf (StringBuilder.)]
-    (doseq [^selmer.node.INode element template]
-      #_(prn element ":" (.render-node element context-map))
-      (if-let [value (.render-node element context-map)]
-        (.append buf value)
-        (.append buf (u/*missing-value-formatter* (:tag (meta element)) context-map))))
-    (.toString buf)))
-
-(defn render
-  " render takes the string, the context-map and possibly also opts. "
-  [s context-map & [opts]]
-  (let [p (parser/parse parser/parse-input
-            (java.io.StringReader. s) opts)]
-    (prn "META " (meta p))
-    (prn "P    " (map meta p))
-    (render-template p context-map)))
-
+(tag/add-tags)
 
 (comment
-  (def s "fff {{a}} {{b}}")
-  (render s {:a 100 :b 'foo}
-    #_{:tag-open (fn [x] (prn "XXX" x) x)}
-    )
-
-  )
+  (def s (prompt-def))
+  (parser/render (:text-analyzer/summarize s)
+    {:text "A very long text to be summarized."
+     :text-type "sentence"}))
---
+++ src/bosquet/template/tag.clj
@@ -0,0 +1,17 @@
+(ns bosquet.template.tag
+  (:require
+   [bosquet.openai :as openai]
+   [clojure.string :as string]
+   [selmer.parser :as parser]))
+
+(defn- args->map [args]
+  (reduce (fn [m arg]
+            (let [[k v] (string/split arg #":")]
+              (assoc m (keyword k) v)))
+    {}
+    args))
+
+(defn add-tags []
+  (parser/add-tag! :llm-generate
+    (fn [args {pretext :selmer/preceding-text}]
+      (openai/complete pretext (args->map args)))))
