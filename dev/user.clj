4419cbc08a3fa0e5fed15835019d3d1ac57a6fc8
Žygimantas Medelis <zygis@hey.com>
2023-01-30 07:11:03 +0200

    update user guide

--- dev/user.clj
+++ dev/user.clj
@@ -2,9 +2,9 @@
   (:require [nextjournal.clerk :as clerk]))
 
 ;; start Clerk's built-in webserver on the default port 7777, opening the browser when done
-(clerk/serve! {:browse? true})
+#_(clerk/serve! {:browse? true})
 
 ;; or let Clerk watch the given `:paths` for changes
-#_(clerk/serve! {:watch-paths ["notebook"]})
+(clerk/serve! {:watch-paths ["notebook"]})
 
 (clerk/show! "notebook/use_guide.clj")
--- notebook/use_guide.clj
+++ notebook/use_guide.clj
@@ -1,8 +1,5 @@
 (ns use-guide
   (:require
-    ;; FIXME this is an issue since openai is not evaluted
-    ;; need to explicitly load it
-    [bosquet.openai]
     [bosquet.generator :as gen]
     [nextjournal.clerk :as clerk]))
 
@@ -10,16 +7,19 @@
 ;;
 ;; ## Simple single template case
 ;;
-;; Let's say we want to generate a synopsis of the play.
+;; Let's say we want to generate a synopsis of the play from
+;; its `title` and the `genre` we want that play to be in.
 ;;
-;; All we know is the `title` of the play and the `genre` we want that play to be in.
-;; Prompt template definition employs Selmer templating library.
+;; ### Selmer templating language
 ;;
-;; [Selmer](https://github.com/yogthos/Selmer) uses `{{DATA}}` syntax to specify where data needs to be injected.
+;; Prompt template definition is done with [Selmer](https://github.com/zmedelis/Selmer) templating library.
+;; It uses `{{DATA}}` syntax to specify where data needs to be injected.
 ;;
 ;; *Bosquet* adds to *Selmer* a specification of where AI generation calls should
-;; happen. This is indicated with the `((bosquet.openai/complete))` construct.
-;; It is a call to a function defined in `bosquet.openai` namespace.
+;; happen. This is indicated with the `{% llm-generate %}` [tag](https://github.com/zmedelis/Selmer#tags).
+;; Behind that tag a completion function defined in `bosquet.openai` namespace will be alled.
+;;
+;; ### Synopsis template
 
 (def synopsis-template
   "You are a playwright. Given the title of play and the desired style,
@@ -29,49 +29,77 @@ Title: {{title}}
 Genre: {{genre}}
 
 Playwright: This is a synopsis for the above play:
-((bosquet.openai/complete))")
+{% llm-generate model=text-davinci-003 var-name=synopsis%}")
 
-;; With the template defined generation can be called.
+;; ### Generation
+;;
 ;; Bosquet will be invoking *OpenAI API* thus make sure that `OPENAI_API_KEY`
 ;; is present as the environment variable.
-
-;; The call to generation function takes in three parameters:
+;;
+;; `llm-generate` call to the *OpenAI* will use configuration parameters specfied
+;; in that tag and reflect parameters specified by [Open AI API](https://beta.openai.com/docs/api-reference/completions).
+;; The tag uses the same names. If config parameters are not used, then defaults
+;; are used. Note that default model is *Ada*, in production *Davinci* would be a
+;; nautural choice.
+;;
+;; ```clojure
+;; {model             ada
+;;  temperature       0.6
+;;  max-tokens        80
+;;  presence-penalty  0.4
+;;  frequence-penalty 0.2
+;;  top-p             1
+;;  n                 1}
+;; ```
+;;
+;; Note the optional `var-name` parameter. This is the name of the var to hold
+;; generation generation result and it can an be used as a reference in other templates
+;; or the same template further down.
+;;
+;; The call to generation function takes in:
 ;; - `template` defined above
 ;; - `data` is the data to fill in the template slots (`title` and `genre`)
-;; optional
-;; - `config` parameter, Bosquet as the default uses Ada model, for more accurate production deployments `{:model "text-davinci-003"}` configuration should be used
 
 ;; The generation comes back with a tuple where the *first* member will contain all
 ;; the text which got its slots filled in and generated completion.
 ;; The *second* member of the tuple will contain only the AI-completed part.
 (def synopsis
   (gen/complete-template
-    synopsis-template {:title "Mr. X" :genre "crime"}))
+    synopsis-template
+    {:title "Mr. X" :genre "crime"}))
 
 ;; Full *Bosquet* produced text
-(clerk/html [:pre (first synopsis)])
+(clerk/html [:code.language-html (first synopsis)])
 
 ;; Just the AI completion part
-(clerk/html [:pre (second synopsis)])
+(clerk/html [:code.language-html (-> synopsis second :synopsis)])
 
 ;; ## Generating from templates with dependencies
 ;;
 ;; With the play synopsis generated, we want to add a review of that play.
 ;; The review prompt template will depend on synopsis generation. With *Bosquet* we
 ;; do not need to worry about resolving the dependencies it will be done automatically.
+;;
 ;; The review prompt template contains familiar call to generation function and
-;; a reference - `{{synopsys-completion}}` (explained bellow) - to generated text for synopsys.
+;; a reference - `{{synopsis-completion.synopsys}}` - to generated text for synopsys.
+;;
+;; The reference points to `synopsis-completion.synopsis` where `synopsis-completion`
+;; points to the map of all completions done by `synopsis` template and `.synopsis`
+;; pics out the `var-name` used for that specific generation.
 
 (def review-template
   "You are a play critic from the New York Times.
 Given the synopsis of play, it is your job to write a review for that play.
 
 Play Synopsis:
-{{synopsis-completion}}
+{{synopsis-completion.synopsis}}
 
 Review from a New York Times play critic of the above play:
-((bosquet.openai/complete))")
+{% llm-generate model=text-davinci-003 %}")
 
+;; Note that `var-name` is not used and generaation will be assigned to a
+;; default `llm-generate` name.
+;;
 ;; Both templates need to be added to a map for further processing
 
 (def play
@@ -82,10 +110,8 @@ Review from a New York Times play critic of the above play:
 ;; *Bosquet* provides the `gen/complete` function taking:
 ;; * `prompts` map defined above
 ;; * `data` to fill in fixed slots (Selmer templating)
-;; * `config` configuration for the generation API
-;; * and the `data-keys` we want to get back (TODO API needs simplification)
 
-(def review (gen/complete play {:title "Mr. X" :genre "crime"} nil))
+(def review (gen/complete play {:title "Mr. X" :genre "crime"}))
 
 ;; Generated review including 'synopsis' generation
-(clerk/html [:pre (:review-completed review)])
+(clerk/html [:code.language-html (:review-completed review)])
