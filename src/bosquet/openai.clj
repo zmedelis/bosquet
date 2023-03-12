1ec25223f5512dd5c588fcaf85b6528d074b09dc
Žygimantas Medelis <zygis@hey.com>
2023-03-12 08:52:07 +0200

    add cgpt-3.5-turbo support

--- src/bosquet/openai.clj
+++ src/bosquet/openai.clj
@@ -6,7 +6,26 @@
 #_:clj-kondo/ignore
 (def davinci "text-davinci-003")
 
+#_:clj-kondo/ignore
+(def cgpt "gpt-3.5-turbo")
+
+
+(defn complete-chat
+  "Completion using cgpt-3.5. This one is loosing the conversation
+  aspect of the API. It will construct basic `system` for the
+  conversation and then use `prompt` as the `user` in the chat "
+  [prompt opts]
+  (-> (assoc opts
+        :model cgpt
+        :messages [{:role "system" :content "You are a helpful assistant."}
+                   {:role "user" :content prompt}])
+    (api/create-chat-completion)
+    :choices first :message :content))
+
+
 (defn complete
+  "Complete `prompt` if passed in `model` is cGPT the completion will
+  be passed to `complete-chat`"
   ([prompt] (complete prompt nil))
   ([prompt {:keys [model temperature max-tokens n top-p
                    presence-penalty frequence-penalty]
@@ -16,17 +35,21 @@
                    presence-penalty  0.4
                    frequence-penalty 0.2
                    top-p             1
-                   n                 1}}]
-   (-> {:model             model
-        :temperature       temperature
-        :max_tokens        max-tokens
-        :presence_penalty  presence-penalty
-        :frequency_penalty frequence-penalty
-        :n                 n
-        :top_p             top-p
-        :prompt            prompt}
-     api/create-completion
-     :choices first :text)))
+                   n                 1}
+            :as opts}]
+   (if (= model cgpt)
+     (complete-chat prompt opts)
+     (-> {:model             model
+          :temperature       temperature
+          :max_tokens        max-tokens
+          :presence_penalty  presence-penalty
+          :frequency_penalty frequence-penalty
+          :n                 n
+          :top_p             top-p
+          :prompt            prompt}
+       api/create-completion
+       :choices first :text))))
 
 (comment
+  (complete "What is your name?" {:max-tokens 10 :model cgpt})
   (complete "1 + 10 ="))
