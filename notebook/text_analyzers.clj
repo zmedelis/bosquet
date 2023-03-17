3e7f27f435ce46b97853ba409b87244c0fdd376d
Žygimantas Medelis <zygis@hey.com>
2023-03-17 19:59:20 +0200

    move prompt palette examples to notebook

--- notebook/text_analyzers.clj
+++ notebook/text_analyzers.clj
@@ -1,7 +1,8 @@
 (ns text-analyzers
   (:require
     [bosquet.converter :as converter]
-    [bosquet.prompt-palette :as pp]))
+    [bosquet.prompt-palette :as pp]
+    [nextjournal.clerk :as clerk]))
 
 ;; ## Text Analyzer prompts
 
@@ -13,23 +14,51 @@
 
 (def sentimental (pp/generator :text-analyzer/extract-fact))
 
-(sentimental
-  {:text-type "tweet"
-   :fact      "sentiment"
-   :text      "How did everyone feel about the Climate Change question last night? Exactly."})
+(def result (sentimental
+              {:text-type "tweet"
+               :fact      "sentiment"
+               :text      "How did everyone feel about the Climate Change question last night? Exactly."}))
 
+;; Tweet sentiment label
+
+(:completion result)
 
 ;; #### Batch sentiment analysis
 
 (def sentimental-batch (pp/generator :text-analyzer/extract-fact-batch))
 
-(def result
-  (sentimental-batch
-    {:text-type "tweets"
-     :fact      "sentiment"
-     :text
-     ["How did everyone feel about the Climate Change question last night? Exactly."
-      "Didn't catch the full #GOPdebate last night. Here are some of Scott's best lines in 90 seconds."
-      "The biggest disappointment of my life came a year ago."]}))
+(def tweets
+  ["How did everyone feel about the Climate Change question last night? Exactly."
+   "Didn't catch the full #GOPdebate last night. Here are some of Scott's best lines in 90 seconds."
+   "The biggest disappointment of my life came a year ago."])
 
-(-> result :facts converter/numbered-items->list)
+{:nextjournal.clerk/visibility {:code :show :result :show}}
+(def batch-result
+  (sentimental-batch {:text-type "tweets"
+                      :fact      "sentiment"
+                      :text      tweets}))
+
+{:nextjournal.clerk/visibility {:code :hide :result :hide}}
+(def sentiment-labels (-> batch-result :completion converter/numbered-items->list vec))
+
+;; Sentiment anlaysis results
+
+{:nextjournal.clerk/visibility {:code :hide :result :show}}
+(clerk/table
+  {"Tweets"    tweets
+   "Sentiment" sentiment-labels})
+
+;; ### Text summarization
+
+(def summarizer (pp/generator :text-analyzer/summarize-to-sentence))
+
+(def summary
+ (summarizer
+   {:text-type "paragraph"
+    :text "OpenAI released its hotly-anticipated GPT-4 on Tuesday,
+providing a 98-page 'technical report' on the latest iteration of its large
+language model (LLM). But despite the lengthy documentation and the company's
+not-for-profit roots, OpenAI has revealed extremely little information about how
+its latest AI actually works — which has experts worried, Venture Beat reports."}))
+
+(clerk/html [:pre (:completion summary)])
--- src/bosquet/prompt_palette.clj
+++ src/bosquet/prompt_palette.clj
@@ -19,27 +19,6 @@
 
 (comment
 
-  (def summarizer (generator :text-analyzer/summarize-to-sentence))
-  (summarizer {:text-type "paragraph"
-               :text      "Once upon the time three things happened."})
-
-
-  (def sentimental (generator :text-analyzer/extract-fact))
-  (sentimental
-    {:text-type "tweet"
-     :fact      "sentiment"
-     :text      "How did everyone feel about the Climate Change question last night? Exactly."})
-
-  (def sentimental-batch (generator :text-analyzer/extract-fact-batch))
-  (sentimental-batch
-    {:text-type "tweets"
-     :fact      "sentiment"
-     :text
-     ["How did everyone feel about the Climate Change question last night? Exactly."
-      "Didn't catch the full #GOPdebate last night. Here are some of Scott's best lines in 90 seconds."
-      "The biggest disappointment of my life came a year ago."]})
-
-
   (def roger-qna
     (generator
       :problem-solver/basic-qna
