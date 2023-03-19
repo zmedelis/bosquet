f86d8a0185967956951df89c13b803c5d52cd41b
Carsten Behring <carsten.behring@gmail.com>
2023-03-19 21:59:28 +0100

    added showcase for split

---
+++ notebook/split.clj
@@ -0,0 +1,45 @@
+(ns test
+  (:require
+   [bosquet.splitter :as splitter]
+   [bosquet.generator :as gen]
+   [nextjournal.clerk :as clerk]))
+(comment
+  (clerk/serve! {:browse? true}))
+
+
+
+
+(def text (slurp "https://raw.githubusercontent.com/scicloj/scicloj.ml.smile/main/LICENSE"))
+
+(def what-template
+  "
+{{text}}
+
+What is the name of the licence ?
+{% llm-generate  model=testtextdavanci003 impl=azure % }")
+
+;;  split the text in pieces
+(def splits
+  (splitter/split-max-tokens text 1000 splitter/heuristic-token-count-fn))
+
+
+;; apply template to all pieces
+(def results
+  (mapv
+   #(gen/complete-template what-template {:text %})
+   splits))
+
+;;  template to summarize the result for all pieces
+(def summarise-all-template
+  "
+{% for t in results %}
+  * {{t}}
+{% endfor %}
+
+Please summarize the above.
+{% llm-generate  model=testtextdavanci003 impl=azure % }
+")
+
+;; apply summarise-all tamplate to get single answer
+(def overall-summary
+  (gen/complete-template summarise-all-template {:results (map first results)}))
