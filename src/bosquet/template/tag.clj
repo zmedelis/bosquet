2b70de1a46e2c33d0dece799ddda7195519cd2e2
Rokas Ramanauskas <rokasramas@gmail.com>
2023-06-28 20:37:52 +0300

    Fix option passing

--- src/bosquet/template/tag.clj
+++ src/bosquet/template/tag.clj
@@ -13,9 +13,7 @@
 (defn add-tags []
   (parser/add-tag! :llm-generate
     (fn [args context]
-      (openai/complete (:selmer/preceding-text context) 
-                       (merge 
-                        (args->map args)
-                        (get-in context [:opts (-> context :the-key) :llm-generate])
-                        
-                        )))))
+      (openai/complete (:selmer/preceding-text context)
+                       (merge
+                         (args->map args)
+                         (:opts context))))))
