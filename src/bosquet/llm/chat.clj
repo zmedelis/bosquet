d19b2a842c186fd9e9b32b55557e345cd8111934
Žygimantas Medelis <zygis@hey.com>
2023-09-16 08:27:50 +0300

    chat helpers

--- src/bosquet/llm/chat.clj
+++ src/bosquet/llm/chat.clj
@@ -8,6 +8,21 @@
   "Key to reference `system` role in ChatML format"
   :bosquet.chat/system)
 
+
+(def role
+  :bosquet.chat/role)
+
+(def content
+  :bosquet.chat/content)
+
+(def user
+  "Key to reference `user` role in ChatML format"
+  :bosquet.chat/user)
+
+(def assistant
+  "Key to reference `assistant` role in ChatML format"
+  :bosquet.chat/assistant)
+
 (def conversation
   "Key to reference complete `conversation` - a history"
   :bosquet.chat/conversation)
@@ -19,3 +34,14 @@
 (def last-message
   "Key to reference `last-message` in the conversation"
   :bosquet.chat/last-message)
+
+(defn speak
+  "Helper function to create a chat message
+
+  ```clojure
+  {:bosquet.chat/role    :assistant
+   :bosqyet.chat/content \"Hello, I am your assistant\"
+  ```"
+  [speaker-role speaker-content]
+  {role    speaker-role
+   content speaker-content})
