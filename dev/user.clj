c3ec672f7c7c3eaa193177b9d5d2df7d827c0760
Žygimantas Medelis <zygis@hey.com>
2023-10-02 20:46:33 +0300

    bb task to genereate docs

--- dev/user.clj
+++ dev/user.clj
@@ -3,14 +3,18 @@
   (:require [nextjournal.clerk :as clerk]
             [portal.api :as p]))
 
+(defn build-static-docs
+  [_]
+  (clerk/build! {:paths    ["notebook/getting_started.clj"
+                            #_"notebook/user_guide.clj"]
+                 :index    "notebook/index.clj"
+                 :out-path "docs"}))
+
 (comment
   (def p (p/open))
   (add-tap #'p/submit)
 
-  (clerk/build! {:paths    ["notebook/getting_started.clj"
-                            #_"notebook/user_guide.clj"]
-                 :index    "notebook/index.clj"
-                 :out-path "docs"})
+
 
   (clerk/serve! {:watch-paths ["notebook"]})
 
