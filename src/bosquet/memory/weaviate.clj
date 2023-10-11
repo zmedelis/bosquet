aab2f799899e9ad1af755a70c43f2dc24fde328f
Žygimantas Medelis <zygis@hey.com>
2023-10-11 06:16:03 +0300

    weaviate experiments

--- src/bosquet/memory/weaviate.clj
+++ src/bosquet/memory/weaviate.clj
@@ -1,9 +1,8 @@
 (ns bosquet.memory.weaviate
   (:import [io.weaviate.client Config WeaviateClient]
-           [io.weaviate.client.base Result]
-           [io.weaviate.client.v1.misc.model Meta]))
+           [io.weaviate.client.v1.graphql.query.argument NearTextArgument]))
 
-(defn client []
+(defn ->client []
   (WeaviateClient. (Config. "http" "localhost:8080")))
 
 (defn status [client]
@@ -16,7 +15,34 @@
         (printf "meta.version: %s\n" (.getVersion res))
         (printf "meta.modules: %s\n" (.getModules res))))))
 
+(defn add [client]
+  (-> client
+    .data
+    .creator
+    (.withClassName "JeopardyQuestion")
+    (.withProperties
+      (hash-map
+        "question" "This vector DB is OSS and supports automatic property type inference on import"
+        "answer" "Weaviate"))
+    (.run)))
+
+(defn search [client q]
+  (-> client
+    (.graphQL)
+    (.get)
+    (.withClassName "JeopardyQuestion")
+    (.withNearText
+      (-> (NearTextArgument/builder)
+        (.concepts [q])
+        (.certainty 0.8)
+        (.build)))
+    (.withFields "question answer _additional { distance }")
+    (.run)))
+
+
 (comment
-  (def c (client))
+  (def c (->client))
   (status c)
+  (add c)
+  (search c "DB OSS")
   #__)
