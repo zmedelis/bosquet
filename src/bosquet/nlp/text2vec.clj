911974b9cc970c8e7493592227259f88d3b120de
Žygimantas Medelis <zygis@hey.com>
2023-10-19 09:03:28 +0300

    backend for DL4J

--- src/bosquet/nlp/text2vec.clj
+++ src/bosquet/nlp/text2vec.clj
@@ -1,13 +1,12 @@
 (ns bosquet.nlp.text2vec
   (:require [clojure.java.io :as io])
   (:import
-    [org.deeplearning4j.models.paragraphvectors ParagraphVectors ParagraphVectors$Builder]
-    [org.deeplearning4j.models.word2vec VocabWord]
+    [org.deeplearning4j.models.paragraphvectors ParagraphVectors$Builder]
     [org.deeplearning4j.models.word2vec.wordstore.inmemory AbstractCache]
     [org.deeplearning4j.text.documentiterator LabelsSource]
-    [org.deeplearning4j.text.sentenceiterator SentenceIterator BasicLineIterator]
+    [org.deeplearning4j.text.sentenceiterator BasicLineIterator]
     [org.deeplearning4j.text.tokenization.tokenizer.preprocessor CommonPreprocessor]
-    [org.deeplearning4j.text.tokenization.tokenizerfactory DefaultTokenizerFactory TokenizerFactory]))
+    [org.deeplearning4j.text.tokenization.tokenizerfactory DefaultTokenizerFactory]))
 
 (defn ->vectors [file-name]
   (let [iterator (BasicLineIterator. (io/file file-name))
@@ -30,7 +29,14 @@
               (.sampling 0.0)
               (.build))]
       (.fit v)
-      (.similarity v "DOC_9835" "DOC_12492"))))
+      v)))
+
+(defn similarity [v doc1 doc2]
+  (.similarity v doc1 doc2))
 
 (comment
-  (->vectors "raw_sentences.txt"))
+  (def v (->vectors "raw_sentences.txt"))
+  (similarity v "DOC_9835" "DOC_12492")
+
+  (.inferVector v "This is a sentence.")
+  )
