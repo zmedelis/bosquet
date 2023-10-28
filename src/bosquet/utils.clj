3ae47be62f9f57df225a96a1f94cd212d9a1e4e0
Žygimantas Medelis <zygis@hey.com>
2023-10-28 21:24:02 +0300

    simple HuggingFace dataset fetcher

--- src/bosquet/utils.clj
+++ src/bosquet/utils.clj
@@ -1,5 +1,26 @@
 (ns bosquet.utils
-  (:import [java.util UUID]))
+  (:require
+   [hato.client :as hc]
+   [jsonista.core :as j])
+  (:import
+   [java.util UUID]))
 
 (defn uuid []
   (UUID/randomUUID))
+
+(def ^:private datasets-server "https://datasets-server.huggingface.co/rows")
+
+(defn hf-dataset [params]
+  (map :row
+       (-> (hc/get datasets-server {:query-params params})
+           :body
+           (j/read-value j/keyword-keys-object-mapper)
+           :rows)))
+
+(comment
+  (hf-dataset
+   {:dataset "stingning/ultrachat"
+    :split   "train"
+    :config  "default"
+    :offset  0
+    :length  10}))
