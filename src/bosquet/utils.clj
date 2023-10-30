2c7f7114f68d0a7619f965fbed0faf124c141d84
Žygimantas Medelis <zygis@hey.com>
2023-10-30 08:01:42 +0200

    reader for downloaded HF dataset

--- src/bosquet/utils.clj
+++ src/bosquet/utils.clj
@@ -20,14 +20,25 @@
 
 ;; TODO. Probably a good candidate for independent lib
 
+(def ^:private default-cache-dir ".cache/hugging-face-datasets")
+
 (defn- write-ds
   [ds-file ds]
   (io/make-parents ds-file)
   (spit ds-file (pp-str ds)))
 
-(defn- ds-file-name
-  [dataset cache-dir]
-  (io/file (str cache-dir "/" (string/replace dataset "/" "_") ".edn")))
+(defn- ds-dir-name [cache-dir dataset]
+  (format "%s/%s"
+          cache-dir
+          (string/replace dataset "/" "_")))
+
+(defn- ds-file
+  ([cache-dir dataset] (ds-file cache-dir dataset 1))
+  ([cache-dir dataset part-nr]
+   (io/file
+    (format "%s/part-%04d.edn"
+            (ds-dir-name cache-dir dataset)
+            part-nr))))
 
 (defn- fetch-hfds [{:keys [dataset offset] :as hf-params}]
   (timbre/infof "Fetching HuggingFace dataset %s (offset=%s)" dataset offset)
@@ -36,21 +47,35 @@
       (j/read-value j/keyword-keys-object-mapper)))
 
 (defn download-ds [{:keys [dataset offset length] :as params}
-                   {:hfds/keys [use-cache cache-dir]
+                   {:hfds/keys [use-cache cache-dir record-limit]
                     :or        {use-cache false cache-dir ".cache/hugging-face-datasets"}}]
   (timbre/infof "Downloading page 1")
-  (let [{:keys [num_rows_total] :as first-page} (fetch-hfds params)]
+  (let [{:keys [num_rows_total] :as first-page} (fetch-hfds params)
+        record-limit (or record-limit num_rows_total)]
     (timbre/info "Total records: " num_rows_total)
-    (write-ds (str (ds-file-name dataset cache-dir) ".page1") first-page)
+    (write-ds (ds-file cache-dir dataset 1) first-page)
     (loop [page 1]
-      (if (> num_rows_total (+ offset (* page length)))
-        (do
-          (timbre/infof "Downloading page %s" (inc page))
-          (write-ds
-           (str (ds-file-name dataset cache-dir) ".page" (inc page))
-           (fetch-hfds (assoc params :offset (+ offset (* page length)))))
-          (recur (inc page)))
-        (timbre/info "Done downloading")))))
+      (let [from-offset (+ offset (* page length))]
+        (if (and
+             (> num_rows_total from-offset)
+             (> record-limit from-offset))
+          (do
+            (timbre/infof "Downloading page %s" (inc page))
+            (write-ds
+             (ds-file cache-dir dataset (inc page))
+             (fetch-hfds (assoc params :offset (+ offset (* page length)))))
+            (recur (inc page)))
+          (timbre/info "Done downloading"))))))
+
+(defn load-ds [ds-dir dataset record-limit]
+  ;; TODO read up to record-limit
+  (let [xf (comp
+            (filter #(.isFile %))
+            (map #(-> % slurp read-string))
+            (mapcat :rows)
+            (map :row))]
+    (into [] xf
+          (file-seq (io/file (ds-dir-name ds-dir dataset))))))
 
 (defn- transform-hfds
   [hfds-raw]
@@ -60,10 +85,10 @@
   ([hf-params] (hf-dataset hf-params nil))
   ([{:keys [dataset] :as hf-params}
     {:hfds/keys [use-cache cache-dir]
-     :or        {use-cache false cache-dir ".cache/hugging-face-datasets"}}]
+     :or        {use-cache false cache-dir default-cache-dir}}]
    (transform-hfds
     (if use-cache
-      (let [cached-ds-file (ds-file-name dataset cache-dir)]
+      (let [cached-ds-file (ds-file dataset cache-dir)]
         (if (.exists cached-ds-file)
            ;; use existing file, do not fetch from HF
           (with-open [rdr (io/reader cached-ds-file)]
@@ -76,13 +101,16 @@
       (fetch-hfds hf-params)))))
 
 (comment
+  (load-ds default-cache-dir "allenai/prosocial-dialog" 1000)
+
   (download-ds
    {:dataset "allenai/prosocial-dialog"
     :split   "train"
     :config  "default"
-    :offset  120000
+    :offset  0
     :length  100}
-   {:hfds/use-cache true})
+   {:hfds/use-cache true
+    :hfds/record-limit 1000})
 
   (hf-dataset
    {:dataset "stingning/ultrachat"
