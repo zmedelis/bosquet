2fd4f913c8f0a984b85ff526223981d589f797c7
Žygimantas Medelis <zygis@hey.com>
2023-10-31 07:56:28 +0200

    Clerk rendering helpers

--- notebook/helpers.clj
+++ notebook/helpers.clj
@@ -1,6 +1,16 @@
 (ns helpers
   (:require
-    [clojure.string :as string]))
+   [clojure.string :as string]
+   [nextjournal.clerk :as clerk]))
+
+(defn wrap-in-el
+  "Wrap collection of HTML elements in another HTML el.
+
+  For example `el = :ul` and `coll = [[:li 1] [:li 2]]
+  =>
+  `[:ul [:li 1] [:li 2]]`"
+  [el coll]
+  (vec (cons el coll)))
 
 (defn kv-cell [k v]
   [:div.pb-2
@@ -12,3 +22,15 @@
 
 (defn join [& lines]
   (apply str (interpose "\n" lines)))
+
+(defn text-div [text]
+  (clerk/html [:div text]))
+
+(defn text-list [coll]
+  (when (seq coll)
+    (clerk/html
+      (wrap-in-el
+        :ul.list-disc
+        (->> coll
+          (remove string/blank?)
+          (mapv #(vector :li %)))))))
