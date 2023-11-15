66d90cf61f46d8d990d11b3aeebd6e9feda12ebe
Žygimantas Medelis <zygis@hey.com>
2023-11-15 06:20:19 +0200

    QnA evaluation set builder

--- src/bosquet/eval/qna_generator.clj
+++ src/bosquet/eval/qna_generator.clj
@@ -1,91 +1,174 @@
 (ns bosquet.eval.qna-generator
   (:require
-   [bosquet.llm.openai-tokens :as otok]
    [bosquet.llm.generator :as gen]
+   [bosquet.llm.openai-tokens :as otok]
    [bosquet.nlp.splitter :as splitter]
-   [bosquet.utils :as utils]
+   [bosquet.read.document :as document]
+   [bosquet.utils :as u]
    [bosquet.wkk :as wkk]
-   [taoensso.timbre :as timbre]))
+   [malli.core :as m]
+   [clojure.java.io :as io]
+   [taoensso.timbre :as timbre]
+   [clojure.core :as c]))
 
 ;; Some of the details are borrowed from
 ;; https://github.com/run-llama/llama_index/blob/29ef306ae0536de44840ca5acfdf93d84b9a560c/llama_index/evaluation/dataset_generation.py
 
+(def query-count :eval/query-count)
+
 (def context-prompt-block
-  (utils/join-nl
+  (u/join-nl
    "CONTEXT INFORMATION is below:"
    "~~~~~~"
    "{{context}}"
    "~~~~~~"))
 
+(def format-constraints
+  #_"Write your response as numbered list, one item per line."
+  (u/join-nl
+   "Write your response in JSON. Resulting JSON is a vector containing generated items one vector element per generated item."
+   "Example JSON output: [\"Item 1\", \"Item 2\", \"Item 3\", \"Item 4\"]"))
+
 (def question-building-prompts
   {:role
-   (utils/join-nl
+   (u/join-nl
     "You are an excelent Teacher who understands subject material perfectly."
     "Your ability to analyze text is astonishing. Based on that your task is to setup"
     "{{question-count}} questions for the upcoming student examination."
     "The questions should be diverse and cover interesting and important topics and facts across the document."
-    "Restrict questions to the CONTEXT INFORMATION provided.")
+    "Restrict the {{question-count}} questions you are writing to the CONTEXT INFORMATION provided.")
 
    :format
-   "Provide questions as string Clojure vector."
+   "Write your response as numbered list, one question per line."
 
-   :question-generation
-   context-prompt-block
+   :question-generation context-prompt-block
 
    :qna
-   (utils/join-nl
+   (u/join-nl
     "{{role}}"
-    ""
     "{{question-generation}}"
     "{{format}}"
+    ""
+    "QUESTIONS:"
     "{% gen var-name=questions%}")})
 
 (def answering-prompt
-  (utils/join-nl
+  (u/join-nl
    context-prompt-block
-   "Given the context information and not prior knowledge, answer the following QUERIES."
-   "QUERIES"
-   "{% for q in queroes %}"
-   "{{forloop.counter}}. {{q}}{% endfor %}"
-   "Answer queries in exact same order as they are listed"
-   "Provide ANSWERS as string Clojure vector."
-   "ANSWERS: {% gen %}"))
+   "Given the CONTEXT INFORMATION and using zero prior knowledge, answer the following QUESTIONS."
+   "QUESTIONS"
+   "{% for q in queries %}"
+   #_"{{forloop.counter}}. {{q}}{% endfor %}"
+   "* {{q}}{% endfor %}"
+   ""
+   "Answer questions in exact same order as they are listed in QUESTIONS. Answer exactly the same list of questions as provided."
+   format-constraints
+   "ANSWERS:"
+   "{% gen %}"))
+
+(def question-set
+  [:sequential [:string {:min 2 :max 400}]])
+
+(def response-set
+  [:sequential [:string {:min 6 :max 1200}]])
+
+(defn- query-response-valid?
+  "Check the validity of questions and responses.
+
+  A naive check see if:
+  * the lenght of question collection is the same as answers
+  * collections are min lenght strings as per schema"
+  [{:keys [queries responses context]}]
+  (if (and
+       (m/validate question-set queries)
+       (m/validate response-set responses)
+       (= (count responses) (count responses)))
+    true
+    (do
+      (timbre/warnf "Query (count:%s) / Response (count: %s) is invalid. Context: '%s'"
+                    (count queries) (count responses)
+                    (u/safe-subs context 0 200))
+      false)))
 
 (defn generate-qna-dataset
-  [document number-of-questions]
+  [{q-count query-count} document]
   (let [chunks       (splitter/text-chunker
-                      {:chunk-size 50 :splitter splitter/sentence-splitter}
+                      {:chunk-size (* q-count 45) :splitter splitter/sentence-splitter}
                       document)
-        model        #_"gpt-4-1106-preview" "gpt-3.5-turbo"
+        model        #_"gpt-3.5-turbo" #_"gpt-4-1106-preview" "gpt-3.5-turbo-1106"
         q-gen-params {:questions
                       {wkk/service          wkk/oai-service
-                       wkk/output-format    :edn
-                       wkk/cache            true
-                       wkk/model-parameters {:temperature 0.2 :max-tokens 500 :model model}}}
+                       wkk/output-format    :list
+                       wkk/model-parameters {:temperature 0.0 :max-tokens (* q-count 100) :model model}}}
         a-gen-params {:gen
                       {wkk/service          wkk/oai-service
-                       wkk/output-format    :edn
-                       wkk/cache            true
-                       wkk/model-parameters {:temperature 0.0 :max-tokens 1500 :model model}}}]
-    (map
-     (fn [chunk]
-       (timbre/debugf "QnA for chunk with token count -  %s" (otok/token-count chunk model))
-       (let [questions (:questions (gen/generate
-                                     question-building-prompts
-                                     {:question-count number-of-questions
-                                      :context         chunk}
-                                     q-gen-params))
-             answers   (:gen (gen/generate
-                               answering-prompt
-                               {:queries questions :context chunk}
-                               a-gen-params))]
-         {:questions questions
-          :answers   answers
-          :context   chunk}))
-     (take 2 chunks))))
+                       wkk/output-format    :json
+                       wkk/model-parameters {:temperature 0.0 :max-tokens (* q-count 400) :model model}}}
+
+        xf (comp
+            (map
+             (fn [chunk]
+               (timbre/debugf "QnA for chunk with token count -  %s" (otok/token-count chunk model))
+               (let [{questions :questions :as g}
+                     (gen/generate
+                      question-building-prompts
+                      {:question-count q-count :context chunk}
+                      q-gen-params)
+
+                     {resp :gen :as answers}
+                     (gen/generate
+                      answering-prompt
+                      {:queries questions :context chunk}
+                      a-gen-params)]
+                 (tap> {'answers answers
+                        'g g})
+                 {:queries questions
+                  :responses resp
+                  :context chunk})))
+             ;; TODO instead of filtering out - retry
+            (filter query-response-valid?))]
+    (into [] xf
+          chunks
+          #_(take 8 chunks))))
+
+(defn qna->eval-dataset
+  "Convert QnA data to a dataset format -  a list of question to answer tuples"
+  [ds-id ds-data]
+  {:dataset ds-id
+   :eval    (vec (mapcat
+                  (fn [{:keys [queries responses]}] (map vector queries responses))
+                  ds-data))})
+
+(defn save-dataset
+  [ds-file ds]
+  (io/make-parents ds-file)
+  (spit ds-file (u/pp-str ds)))
+
+(defn document->dataset
+  [opts document-file dataset-file]
+  (->> document-file
+       document/parse
+       :text
+       (generate-qna-dataset opts)
+       (qna->eval-dataset document-file)
+       (save-dataset dataset-file)))
 
 (comment
-  (def text (:text (bosquet.read.document/parse "data/llama2.pdf")))
+  (document->dataset {query-count 4} "data/llama2.pdf" "data/llama2-eval.edn")
 
-  (def qna (generate-qna-dataset text 5))
-  (tap> qna))
+  (def text (:text (document/parse "data/llama2.pdf")))
+  (def qna (generate-qna-dataset {query-count 3} text))
+  (tap> qna)
+
+  (gen/generate
+   question-building-prompts
+   {:question-count 3
+    :context
+    (second (splitter/text-chunker
+             {:chunk-size 25 :splitter splitter/sentence-splitter}
+             text))}
+   {:questions
+    {wkk/service          wkk/oai-service
+     wkk/output-format    :list
+     wkk/model-parameters {:temperature 0.0 :max-tokens 500 :model "gpt-3.5-turbo-1106"}}})
+  #__)
