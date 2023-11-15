a9e494846eea3c213baaa493abbde172246e0854
Žygimantas Medelis <zygis@hey.com>
2023-11-15 09:09:33 +0200

    evaluation data to embedds

--- src/bosquet/eval/evaluator.clj
+++ src/bosquet/eval/evaluator.clj
@@ -1,4 +1,6 @@
 (ns bosquet.eval.evaluator
+  (:import
+   [bosquet.memory.long_term_memory LongTermMemory])
   (:require
    [bosquet.nlp.splitter :as splitter]
    [bosquet.read.document :as document]
@@ -9,20 +11,24 @@
   (let [memory (sys/get-memory :memory/long-term-embeddings)]))
 
 (defn remember-knowledge
-  [{:keys [memory] :as opts} knowledge]
-  (let [memory (sys/get-memory memory)
-        chunks (splitter/text-chunker
-                {:chunk-size 80 :splitter splitter/sentence-splitter}
-                knowledge)
-        _      (timbre/debugf "Got %s cunks to remember" (count chunks))]
+  [{:keys [storage encoder collection-name] :as opts} knowledge]
+  (let [storage (sys/get-service storage)
+        memory  (LongTermMemory.
+                 storage
+                 (sys/get-service encoder))
+        chunks  (splitter/text-chunker
+                 {:chunk-size 80 :splitter splitter/sentence-splitter}
+                 knowledge)
+        _       (timbre/debugf "Got %s cunks to remember" (count chunks))]
     (.forget memory opts)
+    (.create storage collection-name)
     (doseq [chunk chunks]
-      (.remember memory chunk opts))))
+      (.remember memory {:text chunk} opts))))
 
 (comment
   (def opts {:collection-name "llama2-qna-eval"
-             :memory          :memory/long-term-embeddings})
-
+             :encoder :embedding/openai
+             :storage :db/qdrant})
   (def text (:text (document/parse "data/llama2.pdf")))
 
   (remember-knowledge opts text))
