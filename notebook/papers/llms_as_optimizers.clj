00a1534f2dd9d274ae55f9f771f016522017e291
Žygimantas Medelis <zygis@hey.com>
2023-11-16 19:37:15 +0200

    full OPRO implementation

--- notebook/papers/llms_as_optimizers.clj
+++ notebook/papers/llms_as_optimizers.clj
@@ -18,6 +18,28 @@
 ;; the prompt that contains previously generated solutions with their values, then the new
 ;; solutions are evaluated and added to the prompt for the next optimization step.
 
+
+;;
+;; ## Setup
+;;
+
+(def mem-opts {:collection-name "llama2-qna-eval"
+               :encoder         :embedding/openai
+               :storage         :db/qdrant})
+
+(def text (:text (document/parse "data/llama2.pdf")))
+
+(eval/remember-knowledge mem-opts text)
+
+
+;; Questions and answers golden dataset. Will be used to eval against and optimize the prompts
+(def qna-goldenset
+  (qna/load-qna-dataset "data/llama2-eval.edn"))
+
+;; ##
+;; Prompts
+;;
+
 (def opro-prompt
   (u/join-nl
     "Your task is to generate the instruction <INS>. Below are some previous instructions with their scores."
@@ -25,7 +47,7 @@
     ""
     "{% for i in instruction-score-pairs %}"
     "Instruction (<INS>): {{i.instruction}}"
-    "Score: {{i.score}}"
+    "Score: {{i.avg-score}}"
     "{% endfor %}"
     ""
     "Below we show the task. The <INS> tag is prepended to the below prompt template, e.g. as follows:"
@@ -55,16 +77,6 @@
     "<INS>"
     "{% gen var-name=instruction %}"))
 
-
-(def mem-opts {:collection-name "llama2-qna-eval"
-               :encoder         :embedding/openai
-               :storage         :db/qdrant})
-
-(def text (:text (document/parse "data/llama2.pdf")))
-
-(eval/remember-knowledge mem-opts text)
-
-
 (defn optimization-step-prompt
   [instruction]
   {:instruction     instruction
@@ -80,20 +92,68 @@
                       "{{prompt-template}}"
                       "{% gen var-name=answer %}")})
 
-(def step-0-prompt
+#_(def step-0-prompt
   (optimization-step-prompt "Given the context information and not prior knowledge, answer the query."))
 
-;; Questions and answers golden dataset. Will be used to eval against and optimize the prompts
-(def qna-goldenset
-  (qna/load-qna-dataset "data/llama2-eval.edn"))
 
-(def question
-  (first qna-goldenset))
+(defn evaluate-instruction
+  [step-prompt eval-qna]
+  (map (fn [[eval-question eval-answer]]
+         (let [relevant-memories (eval/query mem-opts eval-question)
+               ;; step-prompt       (optimization-step-prompt instruction-prompt)
+               {:keys [answer]}  (gen/generate
+                                   step-prompt
+                                   {:query   eval-question
+                                    :context relevant-memories}
+                                   {:score wkk/gpt3.5-turbo-with-cache})]
+           {:score         (eval/evaluate-answer eval-question eval-answer answer)
+            :step-prompt   step-prompt
+            :eval-question eval-question
+            :eval-answer   eval-answer
+            :test-answer   answer}))
+    eval-qna))
 
-(def relevant-memories
-  (eval/query mem-opts question))
+(defn average-instruction-score
+  [evaluations]
+  (/ (reduce + (map :score evaluations))
+    (count evaluations)))
 
-(def answer-from-context
+(defn prompt-optimizer
+  [start-prompt eval-set steps]
+  (loop [n            steps
+         step-prompt  start-prompt
+         instructions []]
+    (if (zero? n)
+      instructions
+      (let [step-prompt-template (optimization-step-prompt step-prompt)
+            optimization (gen/generate
+                           opro-prompt
+                           {:instruction-score-pairs instructions
+                            :prompt-template         (:prompt-template step-prompt-template)
+                            :qna-pairs               (take 4 (shuffle qna-goldenset))}
+                           {:score wkk/gpt3.5-turbo-with-cache})
+            eval         (evaluate-instruction
+                           step-prompt-template
+                           #_(:instruction optimization) eval-set)]
+        (tap> {'optimization optimization
+               'eval eval})
+        (recur
+          (dec n)
+          (:instruction optimization)
+          (conj instructions {:instruction (:instruction optimization)
+                              :avg-score   (average-instruction-score eval)
+                              :eval        eval}))))))
+
+(tap> (prompt-optimizer
+        "Given the context information and not prior knowledge, answer the query."
+        (take 5 qna-goldenset)
+        5))
+
+
+#_(def relevant-memories
+    (eval/query mem-opts question))
+
+#_(def answer-from-context
   (:answer
    (gen/generate
      step-0-prompt
@@ -101,23 +161,23 @@
       :context relevant-memories}
      {:score wkk/gpt3.5-turbo-with-cache})))
 
-(def score
+#_(def score
   (eval/evaluate-answer
     question
     "The key contributions of the Llama 2 project include the development and release of pretrained and fine-tuned large language models (LLMs) optimized for dialogue use cases. The Llama 2-Chat models outperform existing open-source chat models on most benchmarks, and based on human evaluations for helpfulness and safety, they may be a suitable substitute for closed-source models."
     answer-from-context))
 
 
-(read/fill-slots
+#_(read/fill-slots
   opro-prompt
   {:instruction-score-pairs [{:instruction (:instruction step-0-prompt) :score score}]
    :prompt-template (:prompt-template step-0-prompt)
    :qna-pairs (take 4 qna-goldenset)})
 
 
-(gen/generate
+#_(gen/generate
   opro-prompt
   {:instruction-score-pairs [{:instruction (:instruction step-0-prompt) :score score}]
    :prompt-template (:prompt-template step-0-prompt)
-   :qna-pairs (take 4 qna-goldenset)}
+   :qna-pairs (take 4 (shuffle qna-goldenset))}
   {:score wkk/gpt3.5-turbo-with-cache})
