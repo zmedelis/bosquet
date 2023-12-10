9174f019b9810758a5f53b1dba97aeb66b534024
Žygimantas Medelis <zygis@hey.com>
2023-12-10 21:33:43 +0200

    price calcs for embeddings

--- src/bosquet/llm/openai_tokens.clj
+++ src/bosquet/llm/openai_tokens.clj
@@ -48,46 +48,57 @@
   - `output` price for the completion tokens
   - `tokens` max context tokens the model supports"
 
-  {:gpt-4             {:input  0.003
-                       :output 0.006
-                       :tokens 8192}
-   :gpt-3.5-turbo-16k {:input  0.003
-                       :output 0.004
-                       :tokens 16384}
-   :gpt-3.5-turbo     {:input  0.0015
-                       :output 0.002
-                       :tokens 4096}
-   :text-ada-001      {:input  0.0004
-                       :output 0.0004
-                       :tokens 2049}
-   :text-babbage-001  {:input  0.0005
-                       :output 0.0005
-                       :tokens 2049}
-   :text-curie-001    {:input  0.002
-                       :output 0.002
-                       :tokens 2049}
-   :text-davinci-003  {:input  0.02
-                       :output 0.02
-                       :tokens 4097}})
+  {:gpt-4                  {:input  0.003
+                            :output 0.006
+                            :tokens 8192}
+   :gpt-3.5-turbo-16k      {:input  0.003
+                            :output 0.004
+                            :tokens 16384}
+   :gpt-3.5-turbo          {:input  0.0015
+                            :output 0.002
+                            :tokens 4096}
+   :text-embedding-ada-002 {:embeddings 0.0001}
+   :text-babbage-002       {:input  0.0004
+                            :output 0.0004
+                            :tokens 16384}
+   :text-curie-001         {:input  0.002
+                            :output 0.002
+                            :tokens 2049}
+   :text-davinci-003       {:input  0.02
+                            :output 0.02
+                            :tokens 4097}})
 
-(defn price-estimate
+(defn- calc-price
+  ([input-price input model]
+   (calc-price input-price 0
+               input (if (string? input) "" 0)
+               model))
+  ([input-price output-price input output model]
+   (cond
+     (and (string? input) (string? output))
+     (+
+      (* (token-count input model) input-price)
+      (* (token-count output model) output-price))
+     ;; If we have got numbers it must be token counts already
+     (and (number? input) (number? output))
+     (+
+      (* input input-price)
+      (* output output-price)))))
+
+(defn generation-price-estimate
   "Estimate price for the `prompt` and `completion` using `model`.
   If `prompt` and `completion` are strings it will count tokens first.
   If `prompt` and `completion` are numbers it will assume they are token counts"
-  ([prompt model] (price-estimate prompt "" model))
+  ([prompt model] (generation-price-estimate prompt "" model))
   ([prompt completion model]
    (let [{:keys [input output]} (model pricing)]
-     ;; If we have got strings count tokens first
-     (cond
-       (and (string? prompt) (string? completion))
-       (+
-        (* (token-count prompt model) input)
-        (* (token-count completion model) output))
-       ;; If we have got numbers it must be token counts already
-       (and (number? prompt) (number? completion))
-       (+
-        (* prompt input)
-        (* completion output))))))
+     (calc-price input output prompt completion model))))
+
+(defn embeddings-price-estimate
+  ([text model] (calc-price
+                 (get-in pricing [model :embeddings])
+                 text model))
+  ([text] (embeddings-price-estimate text :text-embedding-ada-002)))
 
 (defn fits-in-context-window? [token-count model]
   (>= (get-in pricing [model :tokens]) token-count))
--- test/bosquet/llm/openai_tokens_test.clj
+++ test/bosquet/llm/openai_tokens_test.clj
@@ -1,27 +1,31 @@
 (ns bosquet.llm.openai-tokens-test
   (:require
    [clojure.test :refer [deftest is]]
-   [bosquet.llm.openai-tokens :refer [price-estimate encode decode fits-in-context-window?]]))
+   [bosquet.llm.openai-tokens :as tok]))
 
 (deftest encoding-decoding
   (let [txt    "A screaming comes across the sky."
-        tokens (encode txt :gpt-3.5-turbo)]
-    (is (= txt (decode tokens :gpt-3.5-turbo)))))
+        tokens (tok/encode txt :gpt-3.5-turbo)]
+    (is (= txt (tok/decode tokens :gpt-3.5-turbo)))))
 
 (deftest price-estimation
   (is (= (+ (* 7 0.003))
-         (price-estimate
+         (tok/generation-price-estimate
           "A screaming comes across the sky."
           :gpt-4)))
   (is (= (+ (* 7 0.003) (* 15 0.006))
-         (price-estimate
+         (tok/generation-price-estimate
           "A screaming comes across the sky."
           "It has happened before, but there is nothing to compare it to now."
           :gpt-4)))
   (is (= (+ (* 10 0.003) (* 20 0.006))
-         (price-estimate 10 20 :gpt-4))))
+         (tok/generation-price-estimate 10 20 :gpt-4)))
+  (is (= (* 0.0001 1000) (tok/embeddings-price-estimate
+                           ;; make 1k tokens; 'abc' = 1tok
+                          (apply str (take 1000 (repeat "abc"))))))
+  (is (= (* 0.0001 1000) (tok/embeddings-price-estimate 1000))))
 
 (deftest max-tokens
-  (is (fits-in-context-window? 1 :text-ada-001))
-  (is (fits-in-context-window? 2049 :text-ada-001))
-  (is (not (fits-in-context-window? 10000 :text-ada-001))))
+  (is (tok/fits-in-context-window? 1 :text-babbage-002))
+  (is (tok/fits-in-context-window? 2049 :text-babbage-002))
+  (is (not (tok/fits-in-context-window? 20000 :text-babbage-002))))
