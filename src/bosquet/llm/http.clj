37aa0c457e0aa6c818c0e3c5ee4571a0c389dd87
Žygimantas Medelis <zygis@hey.com>
2023-12-14 16:24:38 +0200

    basic support for mistral

--- src/bosquet/llm/http.clj
+++ src/bosquet/llm/http.clj
@@ -2,27 +2,29 @@
   (:require
    [bosquet.utils :as u]
    [clojure.string :as string]
-   [hato.client :as hc]
-   [taoensso.timbre :as timbre]))
+   [hato.client :as hc]))
 
-(defn- fix-params
+
+(def client (hc/build-http-client {:connect-timeout 10000}))
+
+(defn- json-params
   "Snake case keys from `:max-tokens` to `:max_tokens`"
   [params]
-  (reduce-kv
-   (fn [m k v]
-     (assoc m
-            (-> k name (string/replace "-" "_") keyword)
-            v))
-   {}
-   params))
+  (->> params
+    (reduce-kv
+      (fn [m k v]
+        (assoc m
+               (-> k name (string/replace "-" "_") keyword)
+               v))
+      {})
+    u/write-json))
 
 (defn post
-  [{:keys [api-endpoint] :as params}]
-  (timbre/spy
-   (-> api-endpoint
-       (hc/post {:content-type :json
-                 :body         (-> params
-                                   (dissoc :api-endpoint)
-                                   fix-params u/write-json)})
-       :body
-       (u/read-json))))
+  [url {api-key :api-key :as params}]
+  (-> url
+      (hc/post {:content-type :json
+                :oauth-token  api-key
+                :body         (json-params params)
+                :http-client  client})
+      :body
+      (u/read-json)))
