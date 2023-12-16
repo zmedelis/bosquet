f717b6da9fcfc104a590bff9efd0003dc4bdd97d
Žygimantas Medelis <zygis@hey.com>
2023-12-16 17:26:56 +0200

    start revamping llm service use based on handlers

---
+++ src/bosquet/llm.clj
@@ -0,0 +1,48 @@
+(ns bosquet.llm
+  (:require
+   [bosquet.llm.chat :as chat]
+   [bosquet.llm.openai :as openai]))
+
+(def openai ::openai)
+
+(def cohere ::cohere)
+
+(def lm-studio ::lm-studio)
+
+(def azure ::azure)
+
+(def provider ::provider)
+
+(defn- handle-openai-chat [params service-config]
+  (openai/chat-completion service-config params))
+
+(defn- handle-cohere-chat [arg1]
+  )
+
+(defn- handle-lm-studio-chat [arg1]
+  )
+
+(def chat-handlers
+  {openai    handle-openai-chat
+   cohere    handle-cohere-chat
+   lm-studio handle-lm-studio-chat})
+
+(defn chat
+  ([llm-service-config generation-props]
+   (chat llm-service-config generation-props chat-handlers))
+
+  ([{service provider :as llm-config} generation-props handlers]
+   (let [handler (handlers service)]
+     (handler llm-config generation-props))))
+
+
+(comment
+  (chat
+   {provider openai
+    :api-key      (-> "config.edn" slurp read-string :openai-api-key)
+    :api-endpoint "https://api.openai.com/v1"}
+   {:model  :gpt-3.5-turbo
+    :messages (chat/converse chat/user "What is the distance from Moon to Io?")
+    :max-tokens 100
+    :temperature 0.0})
+  #__)
