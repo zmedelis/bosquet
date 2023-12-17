cf814fc2a68cf811aa0ace0ec178322c5590f985
Žygimantas Medelis <zygis@hey.com>
2023-12-17 12:24:00 +0200

    token count for RAG example

--- notebook/examples/rag.clj
+++ notebook/examples/rag.clj
@@ -1,5 +1,9 @@
 (ns examples.rag
   (:require
+   [bosquet.llm.openai-tokens :as tokenizer]
+   [bosquet.system :as system]
+   [bosquet.wkk :as wkk]
+   [clojure.string :as string]
    [hfds-clj.core :as hfds]))
 
 
@@ -8,6 +12,19 @@
                       :config "corpus"
                       :split "corpus"}))
 
+(defn ds->text
+  [ds]
+  (string/join "\n" (map :doc ds)))
+
+(def ds-token-count (tokenizer/token-count (ds->text fiqa-dataset) :text-embedding-ada-002))
+
 ;; 1. Create embeddings
 ;; 2. Create RAG pipeline
 ;; 3. Use RAGA to evaluate the quality
+
+(defn ds->text
+  [ds]
+  (tokenizer/embeddings-price-estimate
+   (string/join "\n" (map :doc ds))))
+
+(def mem (system/get-memory wkk/long-term-embeddings-memory))
