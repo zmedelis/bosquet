e0041415ed43782a54b07e2f0bd3b3c7cbfb2d33
Žygimantas Medelis <zygis@hey.com>
2024-03-24 20:28:52 +0200

    update getting started

--- notebook/getting_started.clj
+++ notebook/getting_started.clj
@@ -1,4 +1,6 @@
+^{:nextjournal.clerk/visibility {:code :fold}}
 (ns getting-started
+  {:nextjournal.clerk/toc true}
   (:require
    [bosquet.llm.generator :as g]
    [bosquet.llm.wkk :as wkk]))
@@ -21,28 +23,42 @@
 (g/generate
  "When I was 6 my sister was half my age. Now I’m 70 how old is my sister?")
 
+;; If you want to change the default, see `config.edn.sample`. Rename it to
+;; `config.edn` and set the values for a default LLM with the desired
+;; parameters.
+
 ;; ### Prompt composition
-;; A use case showing some of the basic Bouquet functionality is using linked prompt templates for text generation.
+;;
+;; A use case showing some of the basic Bouquet functionality with
+;; prompt templates.
 ;;
 ;; Let's say we want AI to generate two texts:
 ;; 1. a synopsis of the play from `title` and `genre` inputs
 ;; 1. a synopsis review.
-
+;;
 ;; Something like this:
-
+;;
 ;; > Title: City of Shadows
 ;; > Genre: Crime
 ;; > Synopsis:
-;; > In the gritty underbelly of City of Shadows, corruption and crime reign supreme. The play follows the interconnected lives of several individuals, revealing the intricate web of deceit and darkness that engulfs the city. Detective Mark Johnson ...
+;; > In the gritty underbelly of City of Shadows, corruption and crime reign
+;; > supreme. The play follows the interconnected lives of several individuals,
+;; > revealing the intricate web of deceit and darkness that engulfs the city.
+;; > Detective Mark Johnson ...
 ;; >
 ;; > Review:
 ;; > Riveting Examination of Corruption and Justice
 ;; > Rating: ★★★★☆
 ;; > ...
 
-;; To do this, let's define a template with a prompt and slots for data insertion. *Bosquet* uses a fork of [Selmer](https://github.com/zmedelis/Selmer) templating library for prompt definitions. *Selmer* provides rich templating language, but for now, all that is needed is its `{{DATA}}` syntax to mark spots in the text for data injection.
-
-;; Linked templates are defined in a map, where the map key is a variable name that can be used to reference templates from each other.
+;; To do this, let's define a template with a prompt and slots for data
+;; insertion. *Bosquet* uses [Selmer](https://github.com/yogthos/selmer)
+;; templating library for prompt definitions. *Selmer* provides rich templating
+;; language, but all what is needed now is its `{{DATA}}` syntax to mark
+;; spots in the text for data injection.
+;;
+;; Linked templates are defined in a map, where the map key is a variable name
+;; that can be used to reference templates from each other.
 
 (def template
   {:synopsis ["You are a playwright. Given the play's title and it's genre"
@@ -62,12 +78,12 @@
               "{{review}}"]
    :review   (g/llm :gpt-4)})
 
-;; Things to note:
+;; **Things to note:**
 ;; * `play` and `review` define generation points, there you specify which LLM to use and which value from the map will be used as prompt context
 ;; * `{{title}}` (and other slots in that form) is where supplied inputs will be injected
-
+;;
 ;; *Bosquet* will be invoking *OpenAI API* thus make sure that `OPENAI_API_KEY` is set in your `secrets.edn`
-
+;;
 ;; With the prerequisite data set, let's run the generation.
 
 ^{:nextjournal.clerk/auto-expand-results? true}
